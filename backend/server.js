ebf6bba21192dba2d4b93954199ef40ebb7f1561
Elliott Chambers <elijr1980@gmail.com>
2022-02-26 21:05:30 -0500

    small fix

--- backend/server.js
+++ backend/server.js
@@ -35,19 +35,6 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/upload", uploadRoutes);
 
-//Build
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-  app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running");
-  });
-}
-
 //Image Uploading
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
@@ -232,6 +219,19 @@ app.post("/create-payment-intent", async (req, res) => {
     paymentIntent: paymentIntent,
   });
 });
+
+//Build
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
 
 //Error Routes
 app.use(notFound);
